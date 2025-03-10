b2536568eeb2f325b27d2258ac510fb7f70ca3fe
borishuu <boris.hutzli@heig-vd.ch>
2025-03-10 16:41:46 +0100

    copied some code from prototype as a base

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,68 @@
-import Image from "next/image";
+'use client'
+
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [quiz, setQuiz] = useState<String>("");
+  const [loading, setLoading] = useState<Boolean>(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+  const generateQuizGemini = async () => {
+    console.log("quiz generation request")
+    setQuiz("")
+    const response = await fetch('/api/gemini/quiz');
+    const data = await response.json();
+    setQuiz(data);
+    console.log("quiz generation completed")
+  }
+
+  const generateQuizGeminiTwoPhase = async () => {
+    console.log("quiz generation request")
+    setQuiz("");
+    setLoading(true);
+    const response = await fetch('/api/gemini/quiz/two-phase');
+    const data = await response.json();
+    console.log(data.context);
+    setLoading(false);
+    setQuiz(data.quiz);
+    console.log("quiz generation completed")
+  }
+
+  const uploadFile = async () => {
+    console.log("upload file request")
+    const response = await fetch('/api/gemini/file');
+    const data = await response.json();
+    console.log(data);
+  }
+
+
+  return (
+    <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col items-center sm:items-start">
+        <button type="button" onClick={uploadFile} className="bg-gray-50 rounded-lg p-2 text-black mb-2">
+          Upload file(s)
+        </button>
+        <button type="button" onClick={generateQuizGeminiTwoPhase} className="bg-gray-50 rounded-lg p-2 text-black mb-2">
+          Generate quiz (two-phase)
+        </button>
+
+        {loading && (
+          <div className="flex flex-col items-center">
+            <img src="/loading.gif" alt="Loading..." className="w-12 h-12 mb-2" />
+            <p>Génération du quiz en cours...</p>
+          </div>
+        )}
+
+        {!loading && quiz !== "" &&
+          quiz.split("\n").map((paragraph: string, index: number) => {
+            const isQuestion = paragraph.trim().match(/^\*\*\d+\./);
+            return (
+              <p key={index} className={isQuestion ? "mt-6 mb-4 font-bold" : "mt-1"}>
+                {paragraph}
+              </p>
+            );
+        })}
+
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
